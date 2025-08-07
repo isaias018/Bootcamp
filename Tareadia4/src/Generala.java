04eb6137ab95818a2afab6e84a10a693be74e584
isaias018 <isaiasjosue098@gmail.com>
2025-08-07 14:57:48 -0300

    Agregue un print

--- Tareadia4/src/Generala.java
+++ Tareadia4/src/Generala.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Generala {
     public static void main(String[] args) {
+        System.out.println("Inicio de progrma");
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
